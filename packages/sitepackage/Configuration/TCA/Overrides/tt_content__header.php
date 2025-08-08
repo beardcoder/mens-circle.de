a19f946cb565d2097762c5962336da3731304ba2
Markus Sommer <markus@letsbenow.de>
2025-08-08 23:17:22 +0200

    feat: migrate header

---
+++ packages/sitepackage/Configuration/TCA/Overrides/tt_content__header.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+call_user_func(static function (): void {
+    $extensionKey = 'sitepackage';
+    $signature = static fn(string $plugin): string => strtolower(
+        sprintf('%s_%s', str_replace('_', '', $extensionKey), $plugin),
+    );
+
+    $key = $signature('Header');
+
+    ExtensionManagementUtility::addRecordType(
+        [
+            'label' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_be.xlf:content.header',
+            'description' => 'LLL:EXT:sitepackage/Resources/Private/Language/locallang_be.xlf:content.header.description',
+            'value' => $key,
+            'icon' => 'tx-sitepackage-content-header',
+            'group' => $extensionKey,
+        ],
+        '
+        header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_formlabel,
+        header_layout;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header_layout_formlabel,
+        bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:appearance,
+        --palette--;;frames,
+        --palette--;;appearanceLinks,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+        categories',
+        [
+            'columnsOverrides' => [
+                'bodytext' => [
+                    'config' => [
+                        'enableRichtext' => true,
+                    ],
+                ],
+            ],
+        ]
+    );
+});
