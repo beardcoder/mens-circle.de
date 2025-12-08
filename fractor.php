91a43479283a637c123a80f73b740a3f4ae68249
Markus Sommer <markus@letsbenow.de>
2025-12-08 22:06:36 +0100

    refactor: standardize Fluid templates and improve accessibility features

--- fractor.php
+++ fractor.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use a9f\Fractor\Configuration\FractorConfiguration;
 use a9f\FractorTypoScript\Configuration\TypoScriptProcessorOption;
 use a9f\Typo3Fractor\Set\Typo3LevelSetList;
@@ -7,7 +9,7 @@ use Helmich\TypoScriptParser\Parser\Printer\PrettyPrinterConditionTermination;
 use Helmich\TypoScriptParser\Parser\Printer\PrettyPrinterConfiguration;
 
 return FractorConfiguration::configure()
-    ->withPaths([__DIR__ . '/packages/'])
+    ->withPaths([__DIR__.'/packages/'])
     ->withOptions([
         TypoScriptProcessorOption::INDENT_SIZE => 2,
         TypoScriptProcessorOption::INDENT_CHARACTER => PrettyPrinterConfiguration::INDENTATION_STYLE_SPACES,
@@ -17,5 +19,6 @@ return FractorConfiguration::configure()
         TypoScriptProcessorOption::CONDITION_TERMINATION => PrettyPrinterConditionTermination::Keep,
     ])
     ->withSets([
-        Typo3LevelSetList::UP_TO_TYPO3_14
-    ]);
+        Typo3LevelSetList::UP_TO_TYPO3_14,
+    ])
+;
