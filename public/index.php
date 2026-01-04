1173546738c6b91529f403989f95ebe587b98497
Markus Sommer <markus@letsbenow.de>
2026-01-04 22:28:07 +0100

    refactor: PHPStan configuration and update index.php for TYPO3 compatibility
    - Removed the inclusion of the TYPO3 PHPStan extension from phpstan.neon.
    - Updated public/index.php to include a new structure for initializing the TYPO3 application.
    - Added license information and improved the handling of the install tool access.
    - Ensured proper application bootstrapping based on the presence of the install tool.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,33 @@
 <?php
 
-declare(strict_types=1);
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
-use TYPO3\CMS\Core\Core\SystemEnvironmentBuilder;
-use TYPO3\CMS\Core\Http\Application;
+call_user_func(static function () {
+    $classLoader = require dirname(__DIR__).'/vendor/autoload.php';
+    \TYPO3\CMS\Core\Core\SystemEnvironmentBuilder::run();
 
-(static function (): void {
-    $classLoader = require dirname(__DIR__) . '/vendor/autoload.php';
-    SystemEnvironmentBuilder::run(0, SystemEnvironmentBuilder::REQUESTTYPE_FE);
-    Application::createFromEnvironment()->run($classLoader);
-})();
+    $isInstallToolDirectAccess = false;
+    if (class_exists(\TYPO3\CMS\Install\Http\Application::class)) {
+        $isInstallToolDirectAccess = isset($_GET['__typo3_install']);
+    }
+
+    $container = \TYPO3\CMS\Core\Core\Bootstrap::init($classLoader, $isInstallToolDirectAccess);
+
+    if ($container->has(\TYPO3\CMS\Core\Http\Application::class)) {
+        $container->get(\TYPO3\CMS\Core\Http\Application::class)->run();
+        return;
+    }
+
+    $container->get(\TYPO3\CMS\Install\Http\Application::class)->run();
+});
